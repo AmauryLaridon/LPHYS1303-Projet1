7d5c6083a1813112c0aac5d9f92a704beb5d9a37
abasilavecch <augustin.basilavecchia@gmail.com>
2021-04-06 15:55:22 +0200

    Màj code et illustrations après correction de la faute de signe

--- ondes.py
+++ ondes.py
@@ -1,23 +1,55 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import sqrt, pi ,exp
+from numpy import sqrt, pi ,exp, cos
 from scipy.stats import hypsecant
 
-
-def onde1(x):
-    return (1/sqrt(2*np.pi*0.01)) * exp(-50*(x-1)**2)
-    
-def onde2(x):
-    return (1/sqrt(2*np.pi*0.4)) * exp(-1.25*(x-5)**2)
-    
-def u0(x):
-    return onde1(x) + onde2(x)
-
 delta = 0.02
+
+
+
+
+# Gaussiennes
+def gauss1(x):
+    return (1/sqrt(2*np.pi*0.1)) * exp(-50*(x-2)**2)
+    
+def gauss2(x):
+    return (1/sqrt(2*np.pi*0.2)) * exp(-25*(x-4)**2)
+    
+def gauss(x):
+    return gauss1(x) + gauss2(x)
+
+
+
+
+
+
+# Solitons
+def soliton1(x,c,a):
+    return 0.5*c*(pi*hypsecant.pdf((x-a)*sqrt(c)/2))**2
+
+def soliton2(x,c,a):
+    return 0.5*c*(pi*hypsecant.pdf((x-a)*sqrt(c)/2))**2
+
+def solit(c1,a1,c2,a2):
+    def sol_(x):
+        return soliton1(x,c1,a1) + soliton2(x,c2,a2)
+    return sol_
+
+
+
+
+# Cosinus
+def cos2(x):
+    return 0.5*cos(0.5*x-0.5*pi)**2
     
 
-def time_ev(u_0, t_f, k, x_f, h):
+
+
+
+
+# Résolution numérique
+def time_ev(u_0, t_f, k, x_f, h, snaps = []):
     x_range = [i*h for i in range(int(x_f/h))]
     U = []
     U.append([u_0(x) for x in x_range])
@@ -31,13 +63,14 @@ def time_ev(u_0, t_f, k, x_f, h):
         else:
             u2 = U[-2]
             
-        U.append([0,0] + [u2[i] - (k/(3*h)) *(u1[i+1] + u1[i] + u1[i-1]) * (u1[i+1] - u1[i-1]) + (delta**2) * (k/(h**3)) * (u1[i+2] - 2*u1[i+1] + 2*u1[i-1] - u1[i-2])  for i in range(2,len(u1)-2)] + [0,0])
-    """plt.plot(x_range, U[0], label = "t=0")
-    plt.plot(x_range, U[100], label = "t=0.1")
-    plt.plot(x_range, U[1000], label = "t=1")
+        U.append([0,0] + [u2[i] - (k/(3*h)) *(u1[i+1] + u1[i] + u1[i-1]) * (u1[i+1] - u1[i-1]) - (delta**2) * (k/(h**3)) * (u1[i+2] - 2*u1[i+1] + 2*u1[i-1] - u1[i-2])  for i in range(2,len(u1)-2)] + [0,0])
+        
+        
+    for s in snaps:
+        plt.plot(x_range, U[int(s/k)], label = "t = {}".format(s))
     plt.legend()
     plt.show()
-    plt.clf()"""
+    plt.clf()
     
     [xx,tt]=np.meshgrid(x_range,t_range)
     plt.contourf(xx,tt, np.array(U), levels = 10)
@@ -47,17 +80,9 @@ def time_ev(u_0, t_f, k, x_f, h):
     plt.show()
     
     
-def soliton1(x,c,a):
-    return 0.5*c*(pi*hypsecant.pdf((x-a)*sqrt(c)/2))**2
-
-def soliton2(x,c,a):
-    return 0.5*c*(pi*hypsecant.pdf((x-a)*sqrt(c)/2))**2
-
-def solit(c1,a1,c2,a2):
-    def sol_(x):
-        return soliton1(x,c1,a1) + soliton2(x,c2,a2)
-    return sol_
     
     
 if __name__ == "__main__":
-    time_ev(solit(20,15,40,5), 6, 0.001, 40, 0.5)
+    #time_ev(gauss, 12, 0.0005, 10, 0.05, [0,4,8,11.5])
+    #time_ev(solit(8,15,16,5), 13, 0.001, 45, 0.5, [0,4,8,12])
+    time_ev(cos2, 8, 0.0001, 2*pi, 0.05, [0,1,4,5.5])
