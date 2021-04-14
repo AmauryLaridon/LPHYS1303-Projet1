ef5a5629fd69bf049535e83ce642542e2a538fb9
abasilavecch <augustin.basilavecchia@gmail.com>
2021-04-14 17:05:52 +0200

    #2 Illustration condition kappa<1

--- Norme_kappa/kappa.py
+++ Norme_kappa/kappa.py
@@ -5,12 +5,12 @@ from numpy import sqrt, pi ,exp, cos, sin
 
 def k(alpha, beta):
     rh = np.arange(0,4*pi, 0.05)
-    kap = [(1-2*alpha*(sin(x/2)**2))**2 + (sin(x)**2)*(alpha - 4*beta*(sin(x/2)**2))**2 for x in rh]
+    kap = [(1+2*alpha*(sin(x/2)**2))**2 + (sin(x)**2)*(alpha - 4*beta*(sin(x/2)**2))**2 for x in rh]
     return max(kap)
 
 
-al = np.arange(-0.08,1.13, 0.05)
-be = np.arange(-0.3,0.73, 0.05)
+al = np.arange(-1.1,0.1, 0.05)
+be = np.arange(-0.7,0.3, 0.05)
 
 kappa = [[k(a,b) for a in al] for b in be]
 
