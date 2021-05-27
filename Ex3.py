47a5cf6edd03fd4e47d601e5fcb2f3c388bb2c17
Amaury Laridon <amaurylaridon@protonmail.com>
2021-05-27 10:13:35 +0200

    Ajout commentaire

--- Ex3.py
+++ Ex3.py
@@ -11,14 +11,15 @@ from Ex1_2 import ZK_KdV, snaps_KdV, contour_KdV
 delta = 0.022
 
 def solit(u1,x1,u2,x2):
+    """Condition initiale de deux solitons."""
     Delta1 = delta/sqrt(u1/12)
     Delta2 = delta/sqrt(u2/12)
     def sol_(x):
         return u1*(pi*hypsecant.pdf((x-x1)/Delta1))**2 + u2*(pi*hypsecant.pdf((x-x2)/Delta2))**2
     return sol_
 
-#Plot semi-animé d'Instantanés
 def inst_semi_anim(u, x_range, param):
+    """Plot semi-animé d'instantanées."""
     t_span = np.arange(0,param[1],0.1)
     n = np.zeros(np.shape(t_span)[0])
     for t in t_span:
@@ -34,32 +35,6 @@ def inst_semi_anim(u, x_range, param):
             plt.pause(0.0001)
             plt.clf()
     plt.close()
-
-    """
-    #Animation 2D
-    x_data = x_range
-    y_data = U
-    U0 = [u_0(x) for x in x_range]
-    #class matplotlib.animation.FuncAnimation(fig, func,
-    #frames = None, init_func=None, fargs=None, save_count = None, *, cache_frame_data = True, **kwargs)
-
-
-    x_0 = 0
-    fig, ax = plt.subplots()
-    ax.set_xlim(x_0,x_f)
-    ax.set_ylim(-5,20)
-    line, = ax.plot(U0)
-
-    def animation_frame(i):
-        x_data.append(i)
-        y_data.append(i)
-        line.set_xdata(x_data)
-        line.set_ydata(y_data)
-        return line,
-
-    animation = FuncAnimation(fig, func=animation_frame, frames = np.arange(0, 10, 0.01), interval = 1)
-    plt.show()
-    """
 
 if __name__ == "__main__":
     #Initialisation ZK dépassement solitons
